2e2c720f8dd397732e9f8260939ccc1e880d65fc
NamMinh1410 <140376336+NamMinh1410@users.noreply.github.com>
2023-07-30 21:36:26 +0700

    Add files via upload

---
+++ js/slider-1.js
@@ -0,0 +1,62 @@
+// JavaScript Document
+$('#sp-keyboards,#sp-keycaps,#sp-pk').owlCarousel({
+	autoplay:true,
+    loop:true,
+    margin:25,
+    nav:true,
+	dots:false,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:2
+        },
+        1000:{
+            items:3
+        }
+    }
+	
+})
+// JavaScript Document
+$('#partner').owlCarousel({
+	autoplay:true,
+    loop:true,
+    margin:25,
+    nav:false,
+	dots:false,
+	
+    responsive:{
+        0:{
+            items:3
+        },
+        600:{
+            items:4
+        },
+        1000:{
+            items:6
+        }
+    }
+	
+})
+// JavaScript Document
+$('#contact').owlCarousel({
+	autoplay:true,
+    loop:true,
+    margin:25,
+    nav:false,
+	dots:false,
+	
+    responsive:{
+        0:{
+            items:3
+        },
+        600:{
+            items:4
+        },
+        1000:{
+            items:6
+        }
+    }
+	
+})
